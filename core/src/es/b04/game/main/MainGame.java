17401e2f6d8c5da6ef12ddd84f73a88a764a330b
Delapee <delape68@gmail.com>
2020-10-29 23:27:27 +0100

    Getters del Vector2 , X e Y, del raton

--- core/src/es/b04/game/main/MainGame.java
+++ core/src/es/b04/game/main/MainGame.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
 public class MainGame extends Game {
+	private static OrthographicCamera camera;
+	private Vector3 mousePosition;
 
 	@Override
 	public void dispose() {
@@ -27,9 +32,15 @@ public class MainGame extends Game {
 	public void render() {
 		super.render();
 
+		//Skin personalizada del raton
 		Pixmap pixmap = new Pixmap(Gdx.files.internal("cursor.png"));
 		Cursor cursor = Gdx.graphics.newCursor(pixmap, 0, 0);
 		Gdx.graphics.setCursor(cursor);
+
+		//Trakeo de la possicion del raton
+		camera.update();
+		mousePosition.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+		MainGame.camera.unproject(mousePosition);
 	}
 
 	@Override
@@ -49,7 +60,22 @@ public class MainGame extends Game {
 
 	@Override
 	public void create() {
+		mousePosition = new Vector3();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		this.setScreen(new MainGameScreen());
 
+	}
+
+	public Vector2 getMousePosition(){
+		return new Vector2(Gdx.input.getX(), Gdx.input.getY());
+	}
+
+	public float getMousePositionX(){
+		return Gdx.input.getX();
+	}
+
+	public float getMousePositionY(){
+		return Gdx.input.getY();
 	}
 }
