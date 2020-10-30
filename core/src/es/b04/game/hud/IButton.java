cc67da5c01d9a64977f1f5569f5c22440db12589
Asier Villar <asiervillar@opendeusto.es>
2020-10-30 23:28:50 +0100

    Re estructuracion del sistema de creacion de botones con la

---
+++ core/src/es/b04/game/hud/IButton.java
@@ -0,0 +1,93 @@
+package es.b04.game.hud;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import es.b04.game.main.MainGameScreen;
+
+public class IButton extends ImageButton {
+
+    private String normal;
+    private String press;
+    private float x;
+    private float y;
+
+    public IButton(String normal, String press, float x, float y){
+        super(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(normal)))),new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(press)))));
+        super.setPosition(x,y);
+    }
+
+    public IButton(String normal, String press, float x, float y, int align){
+        super(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(normal)))),new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(press)))));
+        super.setPosition(x,y,align);
+    }
+
+    public IButton(String normal, String press, float x, float y, ScreenAdapter screen, final Game game) {
+        super(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(normal)))),new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(press)))));
+        super.setPosition(x,y);
+        super.addListener( new ActorGestureListener(){
+            @Override
+            public void tap(InputEvent event, float x, float y, int count, int button) {
+                super.tap(event, x, y, count, button);
+                game.setScreen(new MainGameScreen());
+            }
+
+        });
+    }
+
+    public IButton(String normal, String press, float x, float y, int align, ScreenAdapter screen, final Game game) {
+        super(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(normal)))),new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(press)))));
+        super.setPosition(x,y,align);
+        super.addListener( new ActorGestureListener(){
+            @Override
+            public void tap(InputEvent event, float x, float y, int count, int button) {
+                super.tap(event, x, y, count, button);
+                game.setScreen(new MainGameScreen());
+            }
+
+        });
+    }
+
+    public String getNormal() {
+        return normal;
+    }
+
+    public void setNormal(String normal) {
+        this.normal = normal;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+    @Override
+    public float getX() {
+        return x;
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    @Override
+    public float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+
+}
+
--- core/src/es/b04/game/main/GameMenuScreen.java
+++ core/src/es/b04/game/main/GameMenuScreen.java
@@ -2,23 +2,14 @@ package es.b04.game.main;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import sun.rmi.rmic.Main;
-
-import java.awt.*;
+import es.b04.game.hud.IButton;
 
 public class GameMenuScreen extends ScreenAdapter {
     private Texture tittleBackground;
@@ -41,19 +32,8 @@ public class GameMenuScreen extends ScreenAdapter {
         stage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
         Gdx.input.setInputProcessor(stage);
 
-        playTexture = new Texture(Gdx.files.internal("B1.png"));
-        playPressTexture = new Texture(Gdx.files.internal("B2.png"));
-        ImageButton play = new ImageButton(new TextureRegionDrawable(new TextureRegion(playTexture)), new TextureRegionDrawable(new TextureRegion(playPressTexture)));
-        play.setPosition(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 4f, Align.center);
+        IButton play = new IButton("B1.png","B2.png",Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 4f,Align.center, new MainGameScreen(), game);
         stage.addActor(play);
-
-        play.addListener(new ActorGestureListener() {
-            @Override
-            public void tap(InputEvent event, float x, float y, int count, int button) {
-                super.tap(event, x, y, count, button);
-                game.setScreen(new MainGameScreen());
-            }
-        });
     }
 
     @Override
