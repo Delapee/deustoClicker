3d5c2c98a9175347d9146c3f7ec6350310a57620
Diego Miquelez de Mendiluce <diego.miquelez@opendeusto.es>
2020-11-02 21:45:41 +0100

    Creación enum para los assests

---
+++ core/src/es/b04/game/character/AssetEnum.java
@@ -0,0 +1,15 @@
+package es.b04.game.character;
+
+public enum AssetEnum {
+    PLAY1("B1.png"), PLAY2("B2.png"), GAMEBCK("backgroundOp.png"), SQUADBCK("BackgroundSquad.png"), CENEMY1("C1.png"),
+    CENEMY2("C2.png"), CURSOR("cursor.png"), MAINTITTLE("mainTittle.png");
+    private String asset;
+
+    AssetEnum(String asset) {
+        this.asset = asset;
+    }
+
+    public String getAsset() {
+        return asset;
+    }
+}
