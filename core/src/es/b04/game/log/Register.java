ca250ac0e4981c08c9f5fd28ad363880de92cdd4
Angel de la Peña <angeldelapena@opendeusto.es>
2020-11-02 10:03:01 +0100

    Cambios de variables y margenes de la clase

--- core/src/es/b04/game/log/Register.java
+++ core/src/es/b04/game/log/Register.java
@@ -23,35 +23,36 @@ public class Register extends JFrame {
         this.setLocation( ( screenSize.width - this.getSize().width ) / 2, ( screenSize.height - this.getSize().height ) /2  );
 
         //Superior
-        JPanel sup = new JPanel(new FlowLayout());
-        sup.add(new JLabel("Ventana de Registro DeustoClick"));
+        JPanel top = new JPanel(new FlowLayout());
+        top.add(new JLabel("Ventana de Registro DeustoClick"));
 
         //Medio
         JPanel cen = new JPanel(new GridLayout(3,2,1,1));
-        cen.setBorder(BorderFactory.createEmptyBorder(20,20,10,10));
-        JPanel cen1 = new JPanel(new GridLayout(2,1,1,1));
-        cen1.setBorder(BorderFactory.createEmptyBorder(20,10,10,20));
-        JPanel cen2 = new JPanel(new GridLayout(2,1,1,1));
-        cen2.setBorder(BorderFactory.createEmptyBorder(10,20,10,10));
-        JPanel cen3 = new JPanel(new GridLayout(2,1,1,1));
-        cen3.setBorder(BorderFactory.createEmptyBorder(10,10,10,20));
-        JPanel cen4 = new JPanel(new GridLayout(2,1,1,1));
-        cen4.setBorder(BorderFactory.createEmptyBorder(10,20,10,10));
-        JPanel cen5 = new JPanel(new GridLayout(2,1,1,1));
-        cen5.setBorder(BorderFactory.createEmptyBorder(10,10,20,20));
-        JPanel cen6 = new JPanel(new GridLayout(2,1,1,1));
-        cen6.setBorder(BorderFactory.createEmptyBorder(10,20,20,10));
+        //cen.setBorder(BorderFactory.createEmptyBorder(0,20,0,20));
 
-        final JTextField usuario = new JTextField(20);
-        final JTextField mail = new JTextField(20);
-        final JPasswordField contra = new JPasswordField(20);
-        final JPasswordField contraR = new JPasswordField(20);
-        final JComboBox<String> sexo = new JComboBox<String>();
-        sexo.addItem("Hombre");
-        sexo.addItem("Mujer");
-        sexo.addItem("Otro");
-        final SpinnerNumberModel edadModel = new SpinnerNumberModel(18, 1, 150, 1);
-        final JSpinner edad = new JSpinner(edadModel);
+        JPanel cen1 = new JPanel(new GridLayout(2,1,1,1));
+        cen1.setBorder(BorderFactory.createEmptyBorder(20,20,10,10));
+        JPanel cen2 = new JPanel(new GridLayout(2,1,1,1));
+        cen2.setBorder(BorderFactory.createEmptyBorder(20,10,10,20));
+        JPanel cen3 = new JPanel(new GridLayout(2,1,1,1));
+        cen3.setBorder(BorderFactory.createEmptyBorder(10,20,10,10));
+        JPanel cen4 = new JPanel(new GridLayout(2,1,1,1));
+        cen4.setBorder(BorderFactory.createEmptyBorder(10,10,10,20));
+        JPanel cen5 = new JPanel(new GridLayout(2,1,1,1));
+        cen5.setBorder(BorderFactory.createEmptyBorder(10,20,10,10));
+        JPanel cen6 = new JPanel(new GridLayout(2,1,1,1));
+        cen6.setBorder(BorderFactory.createEmptyBorder(10,10,10,20));
+
+        final JTextField user = new JTextField(20);
+        final JTextField email = new JTextField(20);
+        final JPasswordField pass = new JPasswordField(20);
+        final JPasswordField passR = new JPasswordField(20);
+        final JComboBox<String> gender = new JComboBox<String>();
+        gender.addItem("Hombre");
+        gender.addItem("Mujer");
+        gender.addItem("Otro");
+        final SpinnerNumberModel ageModel = new SpinnerNumberModel(18, 1, 150, 1);
+        final JSpinner age = new JSpinner(ageModel);
 
 
         cen1.add(new JLabel("Nombre de Usuario"));
@@ -61,12 +62,12 @@ public class Register extends JFrame {
         cen5.add(new JLabel("Sexo"));
         cen6.add(new JLabel("Edad"));
 
-        cen1.add(usuario);
-        cen2.add(mail);
-        cen3.add(contra);
-        cen4.add(contraR);
-        cen5.add(sexo);
-        cen6.add(edad);
+        cen1.add(user);
+        cen2.add(email);
+        cen3.add(pass);
+        cen4.add(passR);
+        cen5.add(gender);
+        cen6.add(age);
 
         cen.add(cen1);
         cen.add(cen2);
@@ -77,21 +78,21 @@ public class Register extends JFrame {
 
 
         //Inferior
-        JPanel inf = new JPanel(new FlowLayout());
-        JButton cancelar = new JButton("Cancelar");
-        JButton registrar = new JButton("Registrarse");
-        inf.add(cancelar);
-        inf.add(registrar);
+        JPanel bott = new JPanel(new FlowLayout());
+        JButton cancel = new JButton("Cancelar");
+        JButton register = new JButton("Registrarse");
+        bott.setBorder(BorderFactory.createEmptyBorder(10,20,10,20));
+        bott.add(cancel);
+        bott.add(register);
 
         //Asig final
-        add(BorderLayout.PAGE_START,sup);
+        add(BorderLayout.PAGE_START,top);
         add(BorderLayout.CENTER,cen);
-        add(BorderLayout.PAGE_END,inf);
+        add(BorderLayout.PAGE_END,bott);
 
 
         //Action Listener
-
-        cancelar.addActionListener(new ActionListener() {
+        cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 setVisible(false);
@@ -100,57 +101,57 @@ public class Register extends JFrame {
             }
         });
 
-        registrar.addActionListener(new ActionListener() {
+        register.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //Falta el linkeo con la base de datos
                 boolean todoOk = true;
 
-                if (usuario.getText().equals("")){
-                    usuario.setBorder(new LineBorder(Color.RED, 2));
+                if (user.getText().equals("")){
+                    user.setBorder(new LineBorder(Color.RED, 2));
                     todoOk = false;
                 }else{
-                    usuario.setBorder(new LineBorder(Color.BLACK));
+                    user.setBorder(new LineBorder(Color.BLACK));
                 }
 
-                if (mail.getText().equals("") && !mail.getText().contains("@")){
-                    mail.setBorder(new LineBorder(Color.RED, 2));
+                if (email.getText().equals("") && !email.getText().contains("@")){
+                    email.setBorder(new LineBorder(Color.RED, 2));
                     todoOk = false;
                 }else{
-                    String email = mail.getText();
-                    String[] mailParts = email.split("@");
+                    String mail = email.getText();
+                    String[] mailParts = mail.split("@");
                     if (mailParts.length != 2){
-                        mail.setBorder(new LineBorder(Color.RED, 2));
+                        email.setBorder(new LineBorder(Color.RED, 2));
                         todoOk = false;
                     }else if(!mailParts[1].contains(".") ){
-                        mail.setBorder(new LineBorder(Color.RED, 2));
+                        email.setBorder(new LineBorder(Color.RED, 2));
                         todoOk = false;
                     } else {
-                        mail.setBorder(new LineBorder(Color.BLACK));
+                        email.setBorder(new LineBorder(Color.BLACK));
                     }
 
                 }
 
-                if (String.valueOf(contra.getPassword()).equals("")){
-                    contra.setBorder(new LineBorder(Color.RED, 2));
+                if (String.valueOf(pass.getPassword()).equals("")){
+                    pass.setBorder(new LineBorder(Color.RED, 2));
                     todoOk = false;
                 }else {
-                    contra.setBorder(new LineBorder(Color.BLACK));
+                    pass.setBorder(new LineBorder(Color.BLACK));
                 }
 
-                if (!String.valueOf(contra.getPassword()).equals(String.valueOf(contraR.getPassword()))){
-                    contraR.setBorder(new LineBorder(Color.RED, 2));
+                if (String.valueOf(passR.getPassword()).equals("")){
+                    passR.setBorder(new LineBorder(Color.RED, 2));
                     todoOk = false;
-                }else if (String.valueOf(contraR.getPassword()).equals("")){
-                    contraR.setBorder(new LineBorder(Color.RED, 2));
+                }else if (!String.valueOf(pass.getPassword()).equals(String.valueOf(passR.getPassword()))){
+                    passR.setBorder(new LineBorder(Color.RED, 2));
                     todoOk = false;
                 }else {
-                    contraR.setBorder(new LineBorder(Color.BLACK));
+                    passR.setBorder(new LineBorder(Color.BLACK));
                 }
 
                 if (todoOk){
-                    User u = new User(usuario.getText(), String.valueOf(contraR.getPassword()),
-                                      mail.getText(), String.valueOf(sexo.getSelectedItem()), (Integer) edad.getValue());
+                    User u = new User(user.getText(), String.valueOf(pass.getPassword()),
+                                      email.getText(), String.valueOf(gender.getSelectedItem()), (Integer) age.getValue());
                     check = true;
                     dispose();
                 }
