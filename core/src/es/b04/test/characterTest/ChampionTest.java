42663e7b68376b89e749175bdc437d01a2ebb48d
Asier Villar <asiervillar@outlook.es>
2020-12-21 13:34:06 +0100

    TestChampion

---
+++ core/src/es/b04/test/characterTest/ChampionTest.java
@@ -0,0 +1,44 @@
+package es.b04.test.characterTest;
+
+import es.b04.game.character.Champion;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class ChampionTest {
+
+    private Champion testChamp;
+    @Before
+    public void setUp() throws Exception {
+        ArrayList<String> sChampion = new ArrayList<>();
+        sChampion.add("pirate.png");
+        sChampion.add("pirate.png");
+        testChamp = new Champion(sChampion,"El PIRATA",1,1,100,10,15,33,15,null,null,false);
+    }
+
+    @Test
+    public void testToButton() {
+        assertEquals(1,1);
+        assertEquals(1800,testChamp.getUpgradeGold());
+    }
+
+    @Test
+    public void testUpgradeGold() {
+        assertEquals(1800,testChamp.getUpgradeGold());
+    }
+
+    @Test
+    public void testUpgrade() {
+        testChamp.upgrade();
+        assertEquals(2, testChamp.getLevel());
+        assertEquals((int)(100 * 1.5), testChamp.getDmg());
+        assertEquals((int)(10 * 1.5), testChamp.getAccuracy());
+        assertEquals((int)(15 * 1.5), testChamp.getAttackSpeed());
+        assertEquals(33 * 1.5, testChamp.getCriticProb(),0);
+        assertEquals((int)(15 * 1.5), testChamp.getDodgeProb(),0);
+    }
+}
