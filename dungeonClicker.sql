4f33784ebfe0d15121599d8cadea7025ec30d2df
Angel de la Peña Sarralde <angeldelapena@opendeusto.es>
2021-01-06 12:23:22 +0100

    Actualizada BD y .sql de este para necesedidades actuales

--- dungeonClicker.sql
+++ dungeonClicker.sql
@@ -12,15 +12,9 @@ CREATE TABLE user(
 	expProgress INT(10) NOT NULL,
 	gold INT(10) NOT NULL,
 	autoClick INT(2) NOT NULL,
-	icon VARCHAR(50) NOT NULL);
-    
-CREATE TABLE attack(
-	id VARCHAR(36) PRIMARY KEY NOT NULL,
-	buff INT(5) NOT NULL,
-	debuff INT(5) NOT NULL,
-	cd INT(5) NOT NULL,
-    main BOOL NOT NULL);
-
+	icon VARCHAR(50) NOT NULL,
+    stage INT(2) DEFAULT 1 NOT NULL,
+    stageLevel INT(2) DEFAULT 1 NOT NULL);
 
 CREATE TABLE champion(
 	id VARCHAR(36) PRIMARY KEY NOT NULL,
@@ -38,24 +32,21 @@ CREATE TABLE champion(
 	onSquad BOOL NOT NULL,
     id_user VARCHAR(36) NOT NULL,
     
-    FOREIGN KEY(id_user) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(attackP_id) REFERENCES attack(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(attackS_id) REFERENCES attack(id) ON DELETE CASCADE ON UPDATE CASCADE);
-    
-CREATE TABLE textureChampion(
-	id VARCHAR(36) PRIMARY KEY NOT NULL,
-    name VARCHAR(20) NOT NULL,
-    tipo VARCHAR(20) NOT NULL,
-    img VARCHAR(50) NOT NULL);
-    
-CREATE TABLE textureAttack(
-	id VARCHAR(36) PRIMARY KEY NOT NULL,
-    tipo VARCHAR(20) NOT NULL,
-    img VARCHAR(50) NOT NULL,
-    attack_id VARCHAR(36) NOT NULL,
-    
-    FOREIGN KEY(attack_id) REFERENCES attack(id) ON DELETE CASCADE ON UPDATE CASCADE);
+    FOREIGN KEY(id_user) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE);
 
-INSERT INTO user values ('df9ad349-5936-4fb0-9cfc-d94366c6e7e3', 'admin', 'admin', 'admin@admin.es', 'Hombre', 99, 50,
-                         0, 0, 9999999, 4, 'icon.png');
-    
+CREATE TABLE champDrop(
+    id VARCHAR(36) PRIMARY KEY NOT NULL,
+    name VARCHAR(16) NOT NULL,
+    dmg INT(10) NOT NULL,
+    accuracy DEC(3,2) NOT NULL,
+    attackSpeed DEC(3,2) NOT NULL,
+    criticProb DEC(3,2) NOT NULL,
+    dodgeProb DEC(3,2) NOT NULL);
+
+INSERT INTO user values ('df9ad349-5936-4fb0-9cfc-d94366c6e7e3', 'admin', 'JXMTnpd9uNk=', 'admin@admin.es', 'Hombre', 99, 50,
+                         0, 0, 9999999, 4, 'icon.png', 1, 1);
+
+INSERT INTO champDrop values ('73913408-ded2-4d01-8665-8ceb735f51e2', 'Mercenario', 200, 75, 1,   0.5,  25);
+INSERT INTO champDrop values ('efabd12c-10c2-4093-96ed-eeaf42c49927', 'MPeste',      50, 90, 1,   0.25, 50);
+INSERT INTO champDrop values ('498f2cbf-9347-4558-8ba2-379b7630bdf8', 'Pirata',     150, 70, 1,   0.9,  80);
+INSERT INTO champDrop values ('71c0a811-e4a0-40f5-bdea-09ff76bc902a', 'Mago',       100, 80, 1.5, 0.8,  75);
